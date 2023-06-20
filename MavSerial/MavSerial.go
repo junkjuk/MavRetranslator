c7043a6375bf1b5ab1b77ceb9b472f2c3841250a
Volodymyr Kosinskyi <junkjuk5645@gmail.com>
2023-06-20 09:26:59 +0300

    Add sending to udp

--- MavSerial/MavSerial.go
+++ MavSerial/MavSerial.go
@@ -1,37 +1,78 @@
 package MavSerial
 
-import "github.com/tarm/serial"
+import (
+	"MavRetranslator/MavUtils"
+	"fmt"
+	"github.com/tarm/serial"
+)
 
 type MavSerial struct {
 	serialPort *serial.Port
-	sendBuffer chan []byte
 }
 
 func NewMavSerial(name string, baud int) (*MavSerial, error) {
 	ms := new(MavSerial)
-	ms.sendBuffer = make(chan []byte, 10000)
 	c := &serial.Config{Name: name, Baud: baud}
 	s, err := serial.OpenPort(c)
-	if err == nil {
+	if err != nil {
 		return nil, err
 	}
 	ms.serialPort = s
-	go ms.write()
+
 	return ms, nil
 }
 
 func (ms MavSerial) Read() <-chan []byte {
-	channel := make(chan []byte, 1000)
-	return channel
+	readChan := make(chan []byte, 10000)
+	go func() {
+		for {
+			readCount := 0
+			buff := make([]byte, MavUtils.MavMaxLength)
+			n, _ := ms.serialPort.Read(buff[0:1])
+			readCount += n
+			if buff[0] != MavUtils.Mav2StartByte && buff[0] != MavUtils.Mav1StartByte {
+				continue
+			}
+			isMavlink2 := buff[0] == MavUtils.Mav2StartByte
+			var headerLenght int
+			if isMavlink2 {
+				headerLenght = int(MavUtils.Mav2HeaderLength)
+			} else {
+				headerLenght = int(MavUtils.Mav1HeaderLength)
+			}
+			n, _ = ms.serialPort.Read(buff[readCount : readCount+headerLenght])
+			if n != headerLenght {
+				continue
+			}
+			readCount += n
+			lastLength := int(buff[1] + MavUtils.CrcLength)
+			if isMavlink2 && (buff[2]&MavUtils.MavlinkIflagSigned) > 0 {
+				lastLength += MavUtils.MavlinkSignatureBlockLen
+			}
+
+			n, _ = ms.serialPort.Read(buff[readCount : readCount+lastLength])
+			if n != lastLength {
+				continue
+			}
+			readCount += n
+
+			readChan <- buff[0:readCount]
+		}
+	}()
+	return readChan
 }
 
-func (ms MavSerial) write() error {
-	for {
-		_, err := ms.serialPort.Write(<-ms.sendBuffer)
-		return err
-	}
-}
+func (ms MavSerial) Write() chan<- []byte {
+	sendBuffer := make(chan []byte, 10000)
 
-func (ms MavSerial) Send(packet []byte) {
-	ms.sendBuffer <- packet
+	go func() {
+		for {
+			_, err := ms.serialPort.Write(<-sendBuffer)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		}
+	}()
+	return sendBuffer
 }
--- MavUtils/MavConsts.go
+++ MavUtils/MavConsts.go
@@ -1,5 +1,10 @@
 package MavUtils
 
+const MavMaxLength = 280
+const CrcLength = 2
+const MavlinkIflagSigned = 0x01
+const MavlinkSignatureBlockLen = 13
+
 const Mav2StartByte byte = 0xFD
 const Mav2HeaderLength byte = 9
 
